95e4458704a06bdfdc7d24ddf3f91dd6accfeab6
moreirafelipe1 <felipe.moreira@bllog.com.br>
2024-03-20 10:53:50 -0300

    Add project files.

---
+++ Pendencias/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace Pendencias
+{
+	public class Item
+	{
+		public int id { get; set; }
+		public string assetTypeName { get; set; }
+		public int assetLocationId { get; set; }
+		public DateTime dateTime { get; set; }
+	}
+}
---
+++ Pendencias/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using Pendencias;
+
+internal class Program
+{
+	private static void Main()
+	{
+		var json = File.ReadAllText("Dados.json");
+		List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+		int countRetorno = 0;
+
+		List<Item> roupaLimpa = new List<Item>();
+		List<Item> roupaSuja = new List<Item>();
+
+		foreach (var item in items)
+		{
+			if (item.assetLocationId == 3)
+			{
+				roupaSuja.Add(item);
+			}
+			if (item.assetLocationId == 8)
+			{
+				roupaLimpa.Add(item);
+			}
+		}
+
+		foreach (var item in roupaSuja)
+		{
+			if (item.dateTime < DateTime.Now.AddMinutes(-30))
+			{
+				foreach (var itemLimpo in roupaLimpa)
+				{
+					if (itemLimpo.id == item.id && itemLimpo.dateTime > item.dateTime)
+					{
+						countRetorno++;
+					}
+				}
+			}
+		}
+
+		var countPendencias = roupaSuja.Count - countRetorno;
+		Console.WriteLine(countPendencias);
+	}
+}
